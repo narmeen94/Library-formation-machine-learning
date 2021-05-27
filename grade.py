5dec314b3deebe8428595f993928459c0ccffcc1
ece449 <ece449@ece.iit.edu>
2021-05-27 12:10:54 -0500

    add p5 and p6

--- grade.py
+++ grade.py
@@ -1,18 +1,24 @@
 import traceback
-
+import multiprocessing
 
 def grade_all(name, begin, end, funcs):
     grade = 0
     for q in range(begin, end):
-        func = funcs["grade_Q%d" % q]
+        func = lambda: 0 if funcs["grade_Q%d" % q]() else 1
         try:
-            if func():
-                print("============Q%d passed!============\n" % q)
+            p = multiprocessing.Process(target = func)
+            p.start()
+            p.join(300)
+            if p.is_alive():
+                p.terminate()
+                print("============Q%d timeout!============\n" % q, flush = True)
+            elif p.exitcode == 0:
+                print("============Q%d passed!============\n" % q, flush = True)
                 grade += 1
             else:
-                print("============Q%d failed!============\n" % q)
+                print("============Q%d failed!============\n" % q, flush = True)
         except Exception as e:
-            print("============Q%d failed!============" % q)
+            print("============Q%d failed!============" % q, flush = True)
             print(traceback.format_exc())
     print("Local Testing: %d functions passed" % grade)
     
