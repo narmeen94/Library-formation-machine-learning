bfc9dafe971d632c6b50260197a1ce92214193ea
Narmeen Aamir <naamir@hawk.iit.edu>
2022-11-13 23:45:31 -0600

    flatten done

--- src/eval_flatten.cpp
+++ src/eval_flatten.cpp
@@ -16,28 +16,18 @@ void eval_flatten::eval(vars_type &variables, const kwargs_type &kwargs) //shoul
     int dim=input_flatten.get_dim();
     size_t *shape_flatten=input_flatten.get_shape_array();
     double *data_flatten=input_flatten.get_data_array();
-    std::vector<double>data_new;
+    
 
     size_t N=shape_flatten[0];
     size_t C=shape_flatten[1];
     size_t H=shape_flatten[2];
     size_t W=shape_flatten[3];
 
-    size_t shape_new[2]={N,C*H*W};
 
-
-
-    // size_t N=1;
-    // for(int i=0;i!=dim;i++)
-    // {
-    //     N=N*shape_flatten[i]; //for getting the size of data_array
-    // }
+    size_t shape_new[2]={N,(C*H*W)};
 
     
-
-    //double *data_new_array=data_new.data();
-    
-    variables[expr_id_]=tensor(dim,shape_new,data_flatten);
+    variables[expr_id_]=tensor(2,shape_new,data_flatten);
 
 
 
